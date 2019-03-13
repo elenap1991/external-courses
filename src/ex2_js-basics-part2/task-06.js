beeabb065ec71ac00d841e5f17678889c47bc82d
elena <pirogovael91@gmail.com>
2019-03-13 22:22:26 +0300

    function for primality checking

---
+++ src/ex2_js-basics-part2/task-06.js
@@ -0,0 +1,12 @@
+function checkPrimality (variable){
+    if ((variable > 1000) || (variable < 1)) {
+        return "Данные неверны";
+    }
+    for (var i = 2; i <= Math.sqrt(variable); i++) {
+        if (variable % i === 0) {
+            return ("Число " + variable + " - составное число");
+        }
+    }
+    return ("Число " + variable + " - простое число");
+}
+module.exports = checkPrimality;
