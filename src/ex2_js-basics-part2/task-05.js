a14172cd8a2fea82de9108a9ecb8ea1345eccdaf
elena <pirogovael91@gmail.com>
2019-03-14 16:44:28 +0300

    changed function maxValueInArr (uses for and if)

--- src/ex2_js-basics-part2/task-05.js
+++ src/ex2_js-basics-part2/task-05.js
@@ -1,4 +1,10 @@
 function maxValueInArr(arr) {
-    return Math.max.apply(null, arr);
+    var tmp = arr[0];
+    for (var i = 1; i < arr.length; i++){
+        if (arr[i] > tmp) {
+            tmp = arr[i];
+        }
+    }
+    return tmp;
 }
 module.exports = maxValueInArr;
