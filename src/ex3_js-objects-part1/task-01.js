4510a30116861f1b20ac430d32fa1ff96c0f3f74
elenap1991 <pirogovael91@gmail.com>
2019-03-24 17:45:36 +0300

    added different types properties in object

--- src/ex3_js-objects-part1/task-01.js
+++ src/ex3_js-objects-part1/task-01.js
@@ -1,6 +1,8 @@
 var student = {}
 student.name = "Elena";
 student.age = 27;
-student.adress = "Ruazan, Ruazanskaia";
-student.telNumber = "24-15-16";
+student.adress = null;
+student.assessment = undefined;
+student.driversLicense = false;
+student.telNumber = ["24-15-16","25-25-16"];
 delete student.adress;
