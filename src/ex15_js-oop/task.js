b2400e70c8c2a0ff57be62105a2b961604e7bbf2
elenap1991 <pirogovael91@gmail.com>
2019-05-24 01:48:23 +0300

    improve calculator according to the principles OOP

---
+++ src/ex15_js-oop/task.js
@@ -0,0 +1,7 @@
+const Calculator = require('./StandartCalculator/StandartCalculator.js');
+const EngineerCalculator = require('./EngineerCalculator/EngineerCalculator.js');
+const AccountingCalculator = require('./AccountingCalculator/AccountingCalculator.js');
+
+var standartCalculator = new Calculator();
+var engineerCalculator = new EngineerCalculator();
+var accountingCalculator = new AccountingCalculator();
